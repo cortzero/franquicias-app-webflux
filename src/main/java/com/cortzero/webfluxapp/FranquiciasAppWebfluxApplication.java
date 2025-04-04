890fbbe6fc49a3f31ecb87492871a7f459cc840b
Johan Camilo Cortés Ocampo <camilo4182@gmail.com>
2025-04-03 22:57:45 -0500

    Initialized project

---
+++ src/main/java/com/cortzero/webfluxapp/FranquiciasAppWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.cortzero.webfluxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciasAppWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciasAppWebfluxApplication.class, args);
+	}
+
+}
